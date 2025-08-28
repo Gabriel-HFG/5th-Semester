77111e7b9a5a25664182500aa1a44acd394f54df
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-08-28 16:08:44 +0000

    things

--- third checkpoint/jumps.py
+++ third checkpoint/jumps.py
@@ -1,4 +1,5 @@
 number = int(input("Input a number to start counting: "))
 step_and_start = int(input("input the step size: "))
 
-for i in range(step_and_start, number + 1, step_and_start): print(i)
+for i in range(step_and_start, number + 1, step_and_start):
+    print(i)
