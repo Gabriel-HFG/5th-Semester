8583072bccdf954503551f14cb3bd53909318cd2
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-08-28 16:22:31 +0000

    jumps are bad-

--- third checkpoint/jumps.py
+++ third checkpoint/jumps.py
@@ -1,5 +1,25 @@
-number = int(input("Input a number to start counting: "))
-step_and_start = int(input("input the step size: "))
+def num_check(number1):
+    return number1.isnumeric()
+    
 
-for i in range(step_and_start, number + 1, step_and_start):
-    print(i)
+def main():
+    number = input("Input a number to start counting: ")
+    while not num_check(number):
+        number = input("Enter digit: ")
+
+    while True:
+        step_and_start = input("input the step size: ")
+        while not num_check(step_and_start):
+            step_and_start = input("input the step size: ")
+        if int(step_and_start) == 0:
+            print("number must not be 0")
+        else:
+            break
+
+    step_and_start = int(step_and_start)
+    number = int(number)
+
+    for i in range(step_and_start, number + 1, step_and_start):
+        print(i)
+
+main()
