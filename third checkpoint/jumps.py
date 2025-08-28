a39b34c591e25c969319afe926d18c0d0d292380
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-08-28 16:08:22 +0000

    loops with conditions

---
+++ third checkpoint/jumps.py
@@ -0,0 +1,4 @@
+number = int(input("Input a number to start counting: "))
+step_and_start = int(input("input the step size: "))
+
+for i in range(step_and_start, number + 1, step_and_start): print(i)
