6fbf6bf1d98c49519d1df99c45effc0aea551e9a
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-09-01 19:37:37 +0000

    sabinhg and commit thingds here

---
+++ adv-strings.py
@@ -0,0 +1,27 @@
+# # string input
+string_input = input("Input a string: ")
+# print(f"You entered: {string_input}")
+
+# print("-"*len(string_input))
+
+# #string index
+# print(f"Length of the string: {len(string_input)}")
+# print(f"First two characters: {string_input[0:2]}")
+# print(f"First character: {string_input[0]}")
+# print(f"Last character: {string_input[-1]}")
+
+#Example
+
+# for i in string_input:
+#     print(i)
+
+#other Example
+# i = -1
+# while -i < len(string_input) + 1:
+#     print(string_input[i])
+#     i = i + -1
+
+#Even another example
+print("h" in string_input)
+print("j" in string_input)
+print("ello" in string_input)
