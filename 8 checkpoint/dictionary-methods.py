e3d40f90318d66a0a7cc364e87fc42969b612d1a
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-09-29 19:38:35 +0000

    DICTIONARY METHODs

--- 8 checkpoint/dictionary-methods.py
+++ 8 checkpoint/dictionary-methods.py
@@ -18,3 +18,12 @@ for item in dictionary.items():
 for k, v in dictionary.items():
     print(f"{k} -> {v}")
 
+#get
+picnic_items = {"apples": 5,"cups": 2,}
+print(f"Im bringing {picnic_items.get("cups")} cups")
+print(f"Im bringing {picnic_items.get("eggs", 0)} eggs")
+
+#setting default values
+pet_info = {"name": "Puka", "age": 5}
+pet_info.setdefault("color", "black")
+print(pet_info)
