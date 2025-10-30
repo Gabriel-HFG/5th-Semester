178e9c54999502ce2c34a7f0ddd0e55b9ee62eaa
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-10-30 16:19:53 +0000

    sadsadsadsadsa

---
+++ 10 checkpoint/factorial.py
@@ -0,0 +1,18 @@
+def main():
+    while True:
+        number = int(input("Enter a non-negative integer: "))
+        if number < 0:
+            print("Invalid input. Please enter a non-negative integer.")
+            continue
+        result = factorial(number)
+        print(f"The factorial of {number} is {result}")
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        for i in range(1, n):
+            n *= i
+        return n
+    
+main()
