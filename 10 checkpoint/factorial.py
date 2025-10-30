3320ba95e57ada93221301c08eea24d0135522ad
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-10-30 16:23:21 +0000

    dsadsadsadsadsacxzcx

--- 10 checkpoint/factorial.py
+++ 10 checkpoint/factorial.py
@@ -11,8 +11,9 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     else:
-        for i in range(1, n):
-            n *= i
-        return n
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
     
 main()
