e59fd47028e0764f482f82cc29a9ee80810262e1
Gabriel Fimbres <gabitojuegos2@gmail.com>
2025-11-13 16:33:33 +0000

    dwadsvhythloi,tfygjvyt

--- 12 checkpoint/bitcoin.py
+++ 12 checkpoint/bitcoin.py
@@ -1,11 +1,19 @@
 import requests
 import json
 
-bitcoins = input("Enter your bitcoins")
 api_key = "ed052f6f72151ca6d7f2c4ce6b714002367226c1b34fbbe1a9aa6b836206feea"
+api_key_2 = "a5fcdf69b876e95906642d1ecb78f650"
 
-URL = ("https://rest.coincap.io/v3/assets?search=BTC?apiKey="+api_key)
-info = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+api_key).json()
-print(info["data"]["priceUsd"])
+cryptocurrency = input("Enter the cryptocurrency (bitcoin,etherium,tether,dogecoin): ").lower().strip()
+bitcoins = input(f"Enter {cryptocurrency}:")
+URL = (f"https://rest.coincap.io/v3/assets/{str(cryptocurrency)}?apiKey="+api_key)
+# print(URL)
+info = requests.get(URL).json()
+# print(info)
 price = float(info["data"]["priceUsd"])
-print(f"Total cost of: {bitcoins} is {price * float(bitcoins)}")
+
+# URL = ("https://rest.coincap.io/v3/assets?search=BTC?apiKey="+api_key)
+# info = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey="+api_key).json()
+# price = float(info["data"]["priceUsd"])
+
+print(f"Total cost of {cryptocurrency} coin is {price}\nYou have: {bitcoins} coins\nTotal USD value: ${(price * float(bitcoins)):,.2f}")
